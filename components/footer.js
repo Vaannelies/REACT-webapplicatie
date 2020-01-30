6f33bf9bcac779184e744225c438df0b12536cdc
Vaannelies <0962949@hr.nl>
2020-01-30 17:00:04 +0100

    restart.

--- components/footer.js
+++
@@ -1,9 +0,0 @@
-class Footer extends React.Component {
-    render() {
-        return (
-            <div className="footer">
-                <p>Sweater API: http://145.24.222.55:8000/sweaters</p>
-            </div>
-        )
-    }
-}
--- components/header.js
+++
@@ -1,9 +0,0 @@
-class Header extends React.Component {
-    render() {
-        return (
-            <div className="header">
-                <div><img src="https://fontmeme.com/permalink/200123/2e6fcf7f7d2b266bf9ac2a4e7c8f2a9f.png"/></div>
-            </div>
-        )
-    }
-}
